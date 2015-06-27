0555dc92b1935744dcbd19dd3007d5bf7ca6de13
Wouter de Vries <w.l.devries@gmail.com>
2015-06-27 15:46:57 +0200

    added stuff and setup grunt

---
+++ PictureTalk/Gruntfile.js
@@ -0,0 +1,44 @@
+﻿/// <binding AfterBuild='bowercopy' />
+var grunt = require('grunt');
+
+grunt.initConfig({
+    
+    // configure nodemon
+    nodemon: {
+        dev: {
+            script: './bin/www'
+        }
+    },
+
+    bowercopy: {
+        options: {
+            // Task-specific options go here 
+        },
+        libs: {
+            options: {
+                destPrefix: 'public/js/libs'
+            },
+            files: {
+                'jquery.js': 'jquery/dist/jquery.js',
+                'bootstrap.js': 'bootstrap/dist/js/bootstrap.js'
+            },
+        },
+        css: {
+            options: {
+                destPrefix: 'public/css/libs'
+            },
+            files: {
+                'bootstrap.css': 'bootstrap/dist/css/bootstrap.css',
+                'bootstrap-theme.css': 'bootstrap/dist/css/bootstrap-theme.css'
+            }
+        }
+    }
+
+});
+
+grunt.loadNpmTasks('grunt-nodemon');
+grunt.loadNpmTasks('grunt-bowercopy');
+
+// register the nodemon task when we run grunt
+grunt.registerTask('default', ['nodemon']);
+
